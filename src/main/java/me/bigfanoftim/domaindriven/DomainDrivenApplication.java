67aaa3946f5e3ba2514fcc76392c0091e2f7eeb0
bigfanoftim <bigfanoftim@gmail.com>
2024-02-01 08:38:51 +0900

    feat: initial settings

---
+++ src/main/java/me/bigfanoftim/domaindriven/DomainDrivenApplication.java
@@ -0,0 +1,13 @@
+package me.bigfanoftim.domaindriven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainDrivenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainDrivenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/bigfanoftim/domaindriven/DomainDrivenApplicationTests.java
@@ -0,0 +1,14 @@
+package me.bigfanoftim.domaindriven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+class DomainDrivenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
